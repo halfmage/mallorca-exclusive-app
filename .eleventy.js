cb14878667795e79566fa02d43fae80edb4bf086
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-09-17 18:49:34 +0200

    major updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,7 @@
-require("dotenv").config();
-const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
-const algoliasearch = require("algoliasearch");
-
-const client = algoliasearch(process.env.ALGOLIA_APP, process.env.ALGOLIA_SEARCH_KEY);
-const index = client.initIndex(process.env.ALGOLIA_INDEX);
-
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
-    name: "search", // The serverless function name from your permalink object
-    functionsDir: "./netlify/functions/",
-  });
 
-  eleventyConfig.addFilter("getResults", function (query) {
-    return index.search(query, {
-      attributesToRetrieve: ["allProviders.name", "allProviders.category", "allProviders.description"],
-
-    }).then(res => {
-      return res.hits;
-    })
-  });
-
+    eleventyConfig.addPassthroughCopy({'./node_modules/alpinejs/dist/cdn.js': './js/alpine.js'})
+    
     return {
       markdownTemplateEngine: "njk",
       htmlTemplateEngine: "njk",
