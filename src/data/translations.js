67585c1a8830b74bb3d18d0645cac23d795fd2c9
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-09-19 16:59:54 +0200

    added map view on home

--- src/data/translations.js
+++ src/data/translations.js
@@ -2,11 +2,17 @@ module.exports = {
     en: {
         index: {
             welcome: "Welcome"
-        }
+        },
+        category: "Category",
+        categories: "Categories",
+        allCategories: "All categories"
     },
     de: {
         index: {
             welcome: "Willkommen"
-        }
+        },
+        category: "Kategorie",
+        categories: "Kategorien",
+        allCategories: "Alle Kategorien"
     }
 };
