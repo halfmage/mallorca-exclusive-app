c73fc8d5194339f75359223600aca16dfce6d8f4
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-10-05 14:09:49 +0200

    translations and update

--- src/data/translations.js
+++ src/data/translations.js
@@ -6,7 +6,18 @@ module.exports = {
         all: "All",
         category: "Category",
         categories: "Categories",
-        allCategories: "All categories"
+        allCategories: "All categories",
+        about: "About",
+        imprint: "Imprint",
+        thebest: "The best",
+        onmallorca: "provider on Mallorca",
+        noresultsfound: "No results. Try adjusting your filters.",
+        resetfilter: "Reset All Filters",
+        sortingupdate: "Updated",
+        sortingrating: "Best rated",
+        sortingnewest: "Newest",
+        sortingnearest: "Nearby",
+        searchplaceholder: "Search for anything"
     },
     de: {
         index: {
@@ -15,6 +26,17 @@ module.exports = {
         all: "Alle",
         category: "Kategorie",
         categories: "Kategorien",
-        allCategories: "Alle Kategorien"
+        allCategories: "Alle Kategorien",
+        about: "Über uns",
+        imprint: "Impressum",
+        thebest: "Die besten",
+        onmallorca: "Anbieter auf Mallorca",
+        noresultsfound: "Keine Ergebnisse. Versuchen Sie, Ihre Filter anzupassen.",
+        resetfilter: "Alle Filter zurücksetzen",
+        sortingupdate: "Aktualisiert",
+        sortingrating: "Bestbewertet",
+        sortingnewest: "Neueste",
+        sortingnearest: "In der Nähe",
+        searchplaceholder: "Anbieter durchsuchen"
     }
 };
